71dbc5cc9786c2f5950764c2d55d09b691244407
ahiruguegue0907-prog <ahiruguegue0907@gmail.com>
2026-03-04 15:19:39 +0900

    fix: Add dynamic export to layout to prevent Clerk build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { M_PLUS_Rounded_1c } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+export const dynamic = 'force-dynamic';
+
 const mPlusRounded = M_PLUS_Rounded_1c({
   weight: ["400", "700", "800"],
   subsets: ["latin"],
